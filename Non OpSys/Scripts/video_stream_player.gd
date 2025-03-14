e84009adcc69d993704edab4c4c004717816060d
SmartKirb99 <pokerocraft2@gmail.com>
2025-03-14 13:51:40 -0400

    Documentation

--- Non OpSys/Scripts/video_stream_player.gd
+++ Non OpSys/Scripts/video_stream_player.gd
@@ -1,13 +1,18 @@
 extends VideoStreamPlayer
+##Peak Visuals
+##
+##A video player, what do you expect, VLC?
 
+##The pause/play button
 @onready var btn =$"../Pause"
 
 
-
+##What was the snake that represents looping, but it doesn't like control filled settings, so it's a label
 @onready var Cober = $"../Cober"
+##Sets looping of the video to false
 func _ready() -> void:
 	loop = false
-
+##Pauses/Plays the video, togglable
 func _on_pause_button_down() -> void:
 	paused = !paused
 	if paused:
@@ -15,7 +20,7 @@ func _on_pause_button_down() -> void:
 	else:
 		btn.text = "Pause"
 
-
+##When the loop button is pressed, toggles looping on or off
 func _on_loop_button_down() -> void:
 	print(loop)
 	if loop:
@@ -24,16 +29,16 @@ func _on_loop_button_down() -> void:
 		loop = true
 	Cober.visible = !loop
 
-
+##When the video is finished, if looping, replay the video
 func _on_finished() -> void:
 	if loop:
 		play()
 
-
+## Pops up a file dialog to select a video
 func _on_open_file_pressed() -> void:
 	$"../FileDialog".popup()
 
-
+##Sets up the video
 func _on_file_dialog_file_selected(path):
 	stream.file = path
 	play()
